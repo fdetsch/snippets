9aa35807d059151380b2971b3062f251bf211ef9
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-06-23 09:50:28 +0200

    Update: implemented automatic image storage for LUC comparison plots.

--- evapotranspiration/scintillometer/src/slsCompareLuc.R
+++ evapotranspiration/scintillometer/src/slsCompareLuc.R
@@ -2,13 +2,13 @@
 rm(list = ls(all = TRUE))
 
 switch(Sys.info()[["sysname"]], 
-       "Windows" = setwd("E:/"), 
-       "Linux" = setwd("/media/permanent/"))
+       "Windows" = setwd("F:/kilimanjaro/evapotranspiration"), 
+       "Linux" = setwd("/media/dogbert/XChange/kilimanjaro/evapotranspiration"))
 
 lib <- c("randomForest", "matrixStats", "foreach", "ggplot2", "latticeExtra")
 sapply(lib, function(x) stopifnot(require(x, character.only = TRUE)))
 
-source("phd/scintillometer/src/slsMergeDailyData.R")
+source("scintillometer/src/slsMergeDailyData.R")
 
 srunWorkspaces <- dir("SRun/", pattern = "workspace_SLS", recursive = FALSE, 
                       full.names = TRUE)
@@ -23,7 +23,7 @@ dat <- do.call("rbind", lapply(fls, function(i) {
 }))
 dat$datetime <- strptime(dat$datetime, format = "%Y-%m-%d %H:%M")
 
-png("phd/scintillometer/out/slsCompareLuc.png", width = 25, height = 30, 
+png("scintillometer/out/slsCompareLucHist.png", width = 25, height = 30, 
     units = "cm", res = 300, pointsize = 18)
 ggplot(aes(x = datetime, y = waterET), data = dat) + 
   geom_histogram(stat = "identity") + 
@@ -71,6 +71,8 @@ dat$facet <- factor(dat$facet, levels = c("SAV0 vs. MAI0", "SAV5 vs. MAI4",
 
 dat$luc <- factor(substr(dat$plotid, 1, 3), levels = c("sav", "mai", "gra", "cof"))
 
+png("scintillometer/out/slsCompareLucBoxp.png", width = 25, height = 18, 
+    units = "cm", res = 300, pointsize = 16)
 ggplot(aes(x = hour, y = waterET, fill = luc), data = dat) + 
   geom_boxplot(outlier.colour = NA, notch = TRUE) + 
   facet_wrap(~ facet) +
@@ -80,6 +82,7 @@ ggplot(aes(x = hour, y = waterET, fill = luc), data = dat) +
   ylim(-.05, 1) +
   labs(x = "Hour of day", y = "Evapotranspiration [mm/h]") + 
   theme_bw()
+dev.off()
 
 # Same story, but single plots
 cols <- c("sav0" = "darkgoldenrod", "mai0" = "darkolivegreen", 
@@ -90,34 +93,38 @@ cols <- c("sav0" = "darkgoldenrod", "mai0" = "darkolivegreen",
 foreach(h = list(c("sav0", "mai0"), c("sav5", "mai4"), 
                  c("gra1", "cof3"), c("gra2", "cof2"))) %do% {
                    
-                   dat <- do.call("rbind", lapply(h, function(i) {
-                     # Import current LUC
-                     tmp.fls <- fls[grep(i, fls)]
-                     tmp.dat <- read.csv(tmp.fls)[, c("datetime", "waterET")]
-                     
-                     # Split by day
-                     tmp.dat$time <- substr(tmp.dat$datetime, 12, 19)
-                     tmp.dat.split <- split(tmp.dat, substr(tmp.dat$datetime, 1, 10))
-                     
-                     # Merge by minute of day
-                     tmp.dat.mrg <- Reduce(function(...) merge(..., by = "time"), tmp.dat.split)
-                     tmp.dat.mrg <- tmp.dat.mrg[, -grep("datetime", names(tmp.dat.mrg))]
-                     
-                     # Return rowMedians per minute of day
-                     tmp.mat.mrg <- as.matrix(tmp.dat.mrg[, 2:ncol(tmp.dat.mrg)])
-                     return(data.frame(plotid = i, 
-                                       time = tmp.dat.mrg$time,
-                                       waterET = round(rowMedians(tmp.mat.mrg, na.rm = TRUE), 2)
-                                       #                       waterET = apply(tmp.mat.mrg, 1, FUN = function(...) modal(..., na.rm = TRUE))
-                     ))
-                   }))
-                   
-                   dat$hour <- as.factor(substr(dat$time, 1, 2))
-                   
-                   ggplot(aes(x = hour, y = waterET, fill = plotid), data = dat) + 
-                     geom_boxplot(outlier.colour = NA, notch = TRUE) + 
-                     scale_fill_manual("PlotID", values = cols) +
-                     ylim(-.05, 1) +
-                     labs(x = "Hour of day", y = "Evapotranspiration [mm/h]") + 
-                     theme_bw()
+  dat <- do.call("rbind", lapply(h, function(i) {
+    # Import current LUC
+    tmp.fls <- fls[grep(i, fls)]
+    tmp.dat <- read.csv(tmp.fls)[, c("datetime", "waterET")]
+    
+    # Split by day
+    tmp.dat$time <- substr(tmp.dat$datetime, 12, 19)
+    tmp.dat.split <- split(tmp.dat, substr(tmp.dat$datetime, 1, 10))
+    
+    # Merge by minute of day
+    tmp.dat.mrg <- Reduce(function(...) merge(..., by = "time"), tmp.dat.split)
+    tmp.dat.mrg <- tmp.dat.mrg[, -grep("datetime", names(tmp.dat.mrg))]
+    
+    # Return rowMedians per minute of day
+    tmp.mat.mrg <- as.matrix(tmp.dat.mrg[, 2:ncol(tmp.dat.mrg)])
+    return(data.frame(plotid = i, 
+                      time = tmp.dat.mrg$time,
+                      waterET = round(rowMedians(tmp.mat.mrg, na.rm = TRUE), 2)
+                      #                       waterET = apply(tmp.mat.mrg, 1, FUN = function(...) modal(..., na.rm = TRUE))
+    ))
+  }))
+  
+  dat$hour <- as.factor(substr(dat$time, 1, 2))
+  
+  png(paste0("scintillometer/out/slsCompareLuc_", 
+             paste(h, collapse = "_"), ".png"), 
+      width = 25, height = 16, units = "cm", res = 300, pointsize = 16)
+  print(ggplot(aes(x = hour, y = waterET, fill = plotid), data = dat) + 
+    geom_boxplot(outlier.colour = NA, notch = TRUE) + 
+    scale_fill_manual("PlotID", values = cols) +
+    ylim(-.05, 1) +
+    labs(x = "Hour of day", y = "Evapotranspiration [mm/h]") + 
+    theme_bw())
+  dev.off()
 }
